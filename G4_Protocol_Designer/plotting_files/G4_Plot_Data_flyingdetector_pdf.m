08a1d7cbfc4cd0a061c5951dabe6d5e48287fe8c
Lisa Taylor <taylorl@janelia.hhmi.org>
2019-07-29 13:55:13 -0400

    Updated pdf generation
    Updated plotting files which produce a pdf report to reflect the changes Matt made to the original plotting files.

--- G4_Protocol_Designer/plotting_files/G4_Plot_Data_flyingdetector_pdf.m
+++ G4_Protocol_Designer/plotting_files/G4_Plot_Data_flyingdetector_pdf.m
@@ -1,20 +1,23 @@
-%%%%I have changed the format of the commenting in this file so that only
-%%%%appropriate text shows up in the .pdf file being published from this. -
-%%%%Lisa, 7/10/19
+% This function must be run using the publish command in order to create
+% the .pdf file. The syntax is
+% publish(G4_Plot_Data_flyingdetector(exp_folder, trial_options,
+% metadata),options) where "options" contains the following:
+%options.format = 'pdf';
+%options.outputDir = path to the fly's results folder;
+%options.showCode = false;
 
-function G4_Plot_Data_flyingdetector_pdf(exp_folder, trial_options, metadata_for_publishing, CL_conds, OL_conds, TC_conds)
-%FUNCTION G4_Plot_Data_flyingdetector(exp_folder, trial_options, CL_conds, OL_conds, TC_conds)
-% 
+function G4_Plot_Data_flyingdetector(exp_folder, trial_options, metadata_for_publishing, CL_conds, OL_conds, TC_conds, overlap)
 % Inputs:
 % exp_folder: path containing G4_Processed_Data.mat file
 % trial_options: 1x3 logical array [pre-trial, intertrial, post-trial]
+% metadata_for_publishing: a struct of all metadata from an experiment
+% which displays on the first page of the pdf report.
 % CL_conds: matrix of closed-loop (CL) conditions to plot as histograms
 % OL_conds: matrix of open-loop (OL) conditions to plot as timeseries
 % TC_conds: matrix of open-loop conditions to plot as tuning curves (TC)
-
+% overlap: logical (0 default); plots every 2 rows of conditions on a single row of axes in different colors
 
 %% Metadata
-%%%%%SECTION ADDED BY LISA TAYLOR 7/10/19
 % This function must be published using the publish command in order for
 % the plots to show up in the .pdf. Therefore, any metadata we want in the
 % .pdf must also be part of this function. The disp commands below will
@@ -31,7 +34,7 @@ function G4_Plot_Data_flyingdetector_pdf(exp_folder, trial_options, metadata_for
     disp("Plotting performed?   " + metadata_for_publishing.do_plotting + newline);
     disp("Processing Protocol Used:   " + metadata_for_publishing.processing_protocol + newline);
     disp("Plotting Protocol Used:   " + metadata_for_publishing.plotting_protocol + newline);
-
+    
 %%%% user-defined parameters
 %datatype options: 'LmR_chan', 'L_chan', 'R_chan', 'F_chan', 'Frame Position', 'LmR', 'LpR'
 CL_datatypes = {'Frame Position','LmR','LpR'}; %datatypes to plot as histograms
@@ -39,8 +42,10 @@ OL_datatypes = {'LmR','LpR'}; %datatypes to plot as timeseries
 TC_datatypes = {'LmR','LpR'}; %datatypes to plot as tuning curves
 
 %specify plot properties
-rep_Color = [0.5 0.5 0.5];
-mean_Color = [0 0 0];
+rep_Color = [0.5 0.5 1];
+mean_Color = [0 0 1];
+rep_Color2 = [1 0.5 0.5];
+mean_Color2 = [1 0 0];
 rep_LineWidth = 0.05;
 mean_LineWidth = 1;
 subtitle_FontSize = 8;
@@ -62,18 +67,33 @@ load(fullfile(exp_folder,Data_name));
 
 %create default matrices for plotting all conditions
 if nargin<5 
-    CL_conds = find(Data.conditionModes(:,1)==4); %all closed-loop modes
-    OL_conds = find(Data.conditionModes(:,1)~=4); %all open-loop modes
+    default_W = [2 2 2 2 3 3 3 3 3 3 3 3 4 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5 5]; %width of figure by number of subplots
+    default_H = [1 1 2 2 2 2 3 3 3 4 4 4 4 4 4 4 5 5 5 5 5 5 5 5 5 6 6 6 6 6]; %height of figure by number of subplots
+    
+    %find all open-loop conditions, organize into block
+    conds_vec = find(Data.conditionModes~=4); 
+    num_conds = length(OL_conds_vec);
+    W = default_W(min([num_conds length(default_W)])); %get number of subplot columns (up to default limit)
+    H = default_H(min([num_conds length(default_W)])); %get number of subplot rows
+    D = ceil(num_conds/length(default_W)); %number of figures
+    OL_conds = nan([W H D]);
+    OL_conds(1:num_conds) = conds_vec;
+    OL_conds = permute(OL_conds,[2 1 3]);
+    
+    %find all closed-loop conditions, organize into block
+    conds_vec = find(Data.conditionModes=4); 
+    num_conds = length(CL_conds_vec);
+    W = default_W(min([num_conds length(default_W)])); %get number of subplot columns (up to default limit)
+    H = default_H(min([num_conds length(default_W)])); %get number of subplot rows
+    D = ceil(num_conds/length(default_W)); %number of figures
+    CL_conds = nan([W H D]);
+    CL_conds(1:num_conds) = conds_vec;
+    CL_conds = permute(CL_conds,[2 1 3]);
+    
     TC_conds = []; %by default, don't plot any tuning curves
-    CL_tmp = nan([round(sqrt(numel(CL_conds))) ceil(sqrt(numel(CL_conds)))]);
-    CL_tmp(1:length(CL_conds)) = CL_conds;
-    CL_conds = CL_tmp';
-    %CL_conds = reshape(CL_conds,[round(sqrt(numel(CL_conds))) ceil(sqrt(numel(CL_conds)))])';
-    OL_tmp = nan([round(sqrt(numel(OL_conds))) ceil(sqrt(numel(OL_conds)))]);
-    OL_tmp(1:length(OL_conds)) = OL_conds;
-    OL_conds = OL_tmp';
-    %OL_conds = reshape(OL_conds,[round(sqrt(numel(OL_conds))) ceil(sqrt(numel(OL_conds)))])';
+    overlap = 0; %by default, don't overlap multiple conditions on same plot
 end
+overlap = logical(overlap);
 
 %get datatype indices
 num_OL_datatypes = length(OL_datatypes);
@@ -114,7 +134,7 @@ for d = 1:num_TC_datatypes
     plot(xl,[avg avg],'--','Color',rep_Color','LineWidth',mean_LineWidth)
     title([datastr ' Histogram'],'FontSize',subtitle_FontSize);
     
-    snapnow; %%%LT 7/10
+    snapnow;
 end
 if trial_options(2)==1
     subplot(2,num_TC_datatypes,num_TC_datatypes)
@@ -123,22 +143,22 @@ if trial_options(2)==1
     plot(nanmean(Data.interhistogram),'Color',mean_Color,'LineWidth',mean_LineWidth)
     title('Intertrial Pattern Frame Histogram','FontSize',subtitle_FontSize)
     
-    snapnow; %%%LT 7/10
+    snapnow;
 end
 
-%% plot histograms for closed-loop trials
+%% Histograms for closed-loop trials
 if ~isempty(CL_conds)
     num_figs = size(CL_conds,3);
     for d = CL_inds
         for fig = 1:num_figs
-            num_plot_rows = max(nansum(CL_conds(:,:,fig)>0));
+            num_plot_rows = (1-overlap/2)*max(nansum(CL_conds(:,:,fig)>0));
             num_plot_cols = max(nansum(CL_conds(:,:,fig)>0,2));
-            figure()
+            figure('Position',[100 100 540 540*(num_plot_rows/num_plot_cols)])
             for row = 1:num_plot_rows
                 for col = 1:num_plot_cols
-                    cond = CL_conds(row,col,fig);
-                    if cond>0
-                        subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
+                    cond = CL_conds(row*(1+overlap)-1,col,fig);
+                    if cond>0 
+                        better_subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
                         [~, ~, num_reps, num_positions] = size(Data.histograms);
                         x = circshift(1:num_positions,[1 num_positions/2]);
                         x(x>x(end)) = x(x>x(end))-num_positions;
@@ -149,66 +169,87 @@ if ~isempty(CL_conds)
                         ylim(ylimits(d,:));
                         title(['Condition #' num2str(cond)],'FontSize',subtitle_FontSize)
                         
-                        snapnow;  %%%LT 7/10
+                        
+                    end
+                    if overlap==1
+                        cond = CL_conds(row*(1+overlap),col,fig);
+                        if cond>0
+                            tmpdata = circshift(squeeze(Data.histograms(d,cond,:,:)),[1 num_positions/2]);
+                            plot(repmat(x',[1 num_reps]),tmpdata','Color',rep_Color2,'LineWidth',rep_LineWidth);
+                            plot(x,nanmean(tmpdata),'Color',mean_Color2,'LineWidth',mean_LineWidth)
+                            
+                            
+                        end
                     end
                 end
             end
+            snapnow;
         end
     end
 end
 
-%% plot timeseries data for open-loop trials
+%% Timeseries data for open-loop trials
 if ~isempty(OL_conds)
     num_figs = size(OL_conds,3);
     num_reps = size(Data.timeseries,3);
     %loop for different data types
-    for d = 1:OL_inds
+    for d = OL_inds
         for fig = 1:num_figs
-            num_plot_rows = max(nansum(OL_conds(:,:,fig)>0));
+            num_plot_rows = (1-overlap/2)*max(nansum(OL_conds(:,:,fig)>0));
             num_plot_cols = max(nansum(OL_conds(:,:,fig)>0,2));
-            figure()
+            figure('Position',[100 100 540 540*(num_plot_rows/num_plot_cols)])
             for row = 1:num_plot_rows
                 for col = 1:num_plot_cols
-                    cond = OL_conds(row,col,fig);
-                    if ~isnan(cond)
-                        subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
-                        subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
+                    cond = OL_conds(row*(1+overlap)-1,col,fig);
+                    if cond>0
+                        better_subplot(num_plot_rows, num_plot_cols, col+num_plot_cols*(row-1))
                         plot(repmat(Data.timestamps',[1 num_reps]),squeeze(Data.timeseries(d,cond,:,:))','Color',rep_Color,'LineWidth',rep_LineWidth);
                         hold on
                         plot(Data.timestamps,squeeze(nanmean(Data.timeseries(d,cond,:,:),3)),'Color',mean_Color,'LineWidth',mean_LineWidth);
                         ylim(ylimits(d,:));
                         title(['Condition #' num2str(cond)],'FontSize',subtitle_FontSize)
-                        
-                        snapnow; %%%LT 7/10
+                    end
+                    if overlap==1
+                        cond = CL_conds(row*(1+overlap),col,fig);
+                        if cond>0
+                            plot(repmat(Data.timestamps',[1 num_reps]),squeeze(Data.timeseries(d,cond,:,:))','Color',rep_Color2,'LineWidth',rep_LineWidth);
+                            plot(Data.timestamps,squeeze(nanmean(Data.timeseries(d,cond,:,:),3)),'Color',mean_Color2,'LineWidth',mean_LineWidth);
+                        end
                     end
                 end
             end
             
-            %put snapnow here instead? 
+            snapnow; %Only takes a snapshot every time a figure is full and about to be erased for the next one.
         end
     end
 end
 
-%% plot tuning-curves for specified open-loop trials
+%% Tuning-curves for specified open-loop trials
 if ~isempty(TC_conds)
     num_figs = size(TC_conds,3);
     %loop for different data types
-    for d = 1:TC_inds
+    for d = TC_inds
         for fig = 1:num_figs
-            num_plot_rows = max(nansum(TC_conds(:,:,fig)>0));
-            figure()
+            num_plot_rows = (1-overlap/2)*max(nansum(TC_conds(:,:,fig)>0));
+            figure('Position',[100 100 540/num_plot_rows 540])
             for row = 1:num_plot_rows
-                conds = TC_conds(row,:,fig);
-                conds(isnan(conds)) = [];
-                subplot(num_plot_rows, 1, row)
+                conds = TC_conds(row*(1+overlap)-1,:,fig);
+                conds(isnan(conds)&&conds==0) = [];
+                better_subplot(num_plot_rows, 1, row)
                 plot(squeeze(Data.summaries(d,conds,:)),'Color',rep_Color,'LineWidth',rep_LineWidth);
                 hold on
                 plot(nanmean(Data.summaries(d,conds,:),3),'Color',mean_Color,'LineWidth',mean_LineWidth);
                 ylim(ylimits(d,:));
                 title(['Condition #' num2str(cond)],'FontSize',subtitle_FontSize)
-                
-                snapnow; %%%LT 7/10
+                if overlap==1
+                    conds = TC_conds(row*(1+overlap),:,fig);
+                    conds(isnan(conds)&&conds==0) = [];
+                    plot(squeeze(Data.summaries(d,conds,:)),'Color',rep_Color2,'LineWidth',rep_LineWidth);
+                    plot(nanmean(Data.summaries(d,conds,:),3),'Color',mean_Color2,'LineWidth',mean_LineWidth);
+                end
             end
+            
+            snapnow; %only snapshots each figure once - make sure this isn't missing anything!
         end
     end
 end
