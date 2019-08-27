23378ed777a301eb96efcea0215b840290574952
Lisa Taylor <taylorl@janelia.hhmi.org>
2019-08-27 16:31:39 -0400

    Update recently_opened_g4p_files.m
    reset file that tracks recently opened .g4p files

--- G4_Protocol_Designer/support_files/recently_opened_g4p_files.m
+++ G4_Protocol_Designer/support_files/recently_opened_g4p_files.m
@@ -1,4 +1,4 @@
-C:\Users\taylorl\Desktop\tester08-26-19_16-48-28\tester08-26-19_16-48-28.g4p
-C:\Users\taylorl\Desktop\opened_first08-27-19_16-16-56\opened_first08-27-19_16-16-56.g4p
-C:\Users\taylorl\Desktop\opened_last08-27-19_16-19-20\opened_last08-27-19_16-19-20.g4p
-C:\Users\taylorl\Desktop\test08-23-19_12-42-37\test08-23-19_12-42-37.g4p
+   
+   
+   
+   
