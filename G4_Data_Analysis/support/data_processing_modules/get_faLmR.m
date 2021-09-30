94f50af64cf76d5689270870d14e530b042c1b87
Lisa Taylor <45794967+taylorlm88@users.noreply.github.com>
2021-09-30 17:05:03 -0400

    bug fixed in obtaining flipped and averaged data

--- G4_Data_Analysis/support/data_processing_modules/get_faLmR.m
+++ G4_Data_Analysis/support/data_processing_modules/get_faLmR.m
@@ -1,26 +1,41 @@
 function [faLmR_data, faLmR_data_norm] = get_faLmR(ts_data, ts_norm, LmR_ind, condition_pairs)
     
+    num_conds = size(ts_data, 2);
+    
+    if isempty(condition_pairs)
+        pair = 1;
+        for con = 1:2:num_conds
+            if con + 1 > num_conds
+                condition_pairs{pair} = [];
+            else
+                condition_pairs{pair} = [con con+1];
+            end
+            pair = pair + 1;
+        end
+    end
+            
+
     faLmR_data = NaN(length(condition_pairs), size(ts_data,3),  size(ts_data,4));
     faLmR_data_norm = NaN(length(condition_pairs), size(ts_data,3),  size(ts_data,4));
-
-    if isempty(condition_pairs)
-        
-        for new = 1:length(condition_pairs)
-            tmp(1, :,:,:,:) = -ts_data(LmR_ind, condition_pairs{new}(2), :, :);
-            tmp(2, :,:,:,:) = ts_data(LmR_ind, condition_pairs{new}(1), :, :);
-            
-            new_condition = nanmean(tmp,1);
-            faLmR_data(new,:,:) = squeeze(new_condition);
-            
-            tmpNorm(1,:,:,:,:) = -ts_norm(LmR_ind, condition_pairs{new}(2), :, :);
-            tmpNorm(2, :, :, :, :) = ts_norm(LmR_ind, condition_pairs{new}(1), :, :);
-            
-            new_condition_norm = nanmean(tmpNorm,1);
-            faLmR_data_norm(new, :, :) = squeeze(new_condition_norm);
-        end
-        
-        
-    else
+% 
+%     if isempty(condition_pairs)
+%         
+%         for new = 1:length(condition_pairs)
+%             tmp(1, :,:,:,:) = -ts_data(LmR_ind, condition_pairs{new}(2), :, :);
+%             tmp(2, :,:,:,:) = ts_data(LmR_ind, condition_pairs{new}(1), :, :);
+%             
+%             new_condition = nanmean(tmp,1);
+%             faLmR_data(new,:,:) = squeeze(new_condition);
+%             
+%             tmpNorm(1,:,:,:,:) = -ts_norm(LmR_ind, condition_pairs{new}(2), :, :);
+%             tmpNorm(2, :, :, :, :) = ts_norm(LmR_ind, condition_pairs{new}(1), :, :);
+%             
+%             new_condition_norm = nanmean(tmpNorm,1);
+%             faLmR_data_norm(new, :, :) = squeeze(new_condition_norm);
+%         end
+%         
+%         
+%     else
         
         for newc = 1:length(condition_pairs)
             tmp_data(1, :, :, :, :) = -ts_data(LmR_ind, condition_pairs{newc}(2), :, :);
@@ -38,7 +53,7 @@ function [faLmR_data, faLmR_data_norm] = get_faLmR(ts_data, ts_norm, LmR_ind, co
 
         end
         
-    end
+%     end
 
 
 end
