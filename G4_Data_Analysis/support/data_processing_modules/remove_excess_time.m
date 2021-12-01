c475ee886345142f1b7c9bf1c583d0654c76bc12
Lisa Taylor <45794967+taylorlm88@users.noreply.github.com>
2021-12-01 12:37:46 -0500

    tested without fly and made updates to reduce duration errors.

--- G4_Data_Analysis/support/data_processing_modules/remove_excess_time.m
+++ G4_Data_Analysis/support/data_processing_modules/remove_excess_time.m
@@ -18,62 +18,48 @@ function [Log, stop_times] = remove_excess_time(Log, start_times, stop_times, st
     timestamp_diff = Log.ADC.Time(1,2)-Log.ADC.Time(1,1);
     frame_timestamp_diff = Log.Frames.Time(2) - Log.Frames.Time(1);
     postTrialTimes = postTrialTimes*time_conv;
-    for time = 2:length(start_times)-1
+     for time = 2:length(start_times)-1
         if time <= length(postTrialTimes)/2
-            time1 = postTrialTimes(time*2-1);
-            time2 = postTrialTimes(time*2);
-            num_inds_erase1 = time1/timestamp_diff;
-            num_inds_erase2 = time2/timestamp_diff;
-            frame_inds_erase1 = time1/frame_timestamp_diff;
-            frame_inds_erase2 = time2/frame_timestamp_diff;
-            start_time_ind = find(Log.ADC.Time(1,:)>=(start_times(time)*time_conv),1);
-            frame_start_ind = find(Log.Frames.Time(:)>=(start_times(time)*time_conv),1);
-            next_trial_start_time_ind = find(Log.ADC.Time(1,:)>=(start_times(time+1)*time_conv),1);
-            next_trial_frame_start_ind = find(Log.Frames.Time(:)>=(start_times(time+1)*time_conv),1);
+            time1 = postTrialTimes(time*2);
+            time2 = postTrialTimes(time*2 + 1);
+            totTime = time1 + time2;
+            num_inds_erase = totTime/timestamp_diff;
+            
+            frame_inds_erase = totTime/frame_timestamp_diff;
+ 
+            start_time_ind = find(Log.ADC.Time(1,:)>=(start_times(time)),1);
+            frame_start_ind = find(Log.Frames.Time(:)>=(start_times(time)),1);
+ 
             %remove data corresponding to the amount of time in time1 from
             %data directly preceding the start time (so remove it from the
             %end of the previous trial)
-            ind11 = start_time_ind - num_inds_erase1;
-            ind12 = start_time_ind - 1;
-            frameInd11 = frame_start_ind - frame_inds_erase1;
-            frameInd12 = frame_start_ind - 1;
+            ind1 = start_time_ind - num_inds_erase;
+            ind2 = start_time_ind - 1;
+            frameInd1 = frame_start_ind - frame_inds_erase;
+            frameInd2 = frame_start_ind - 1;
         
-            Log.Frames.Time(frameInd11:frameInd12) = [];
-            Log.Frames.Position(frameInd11:frameInd12) = [];
-            Log.ADC.Time(:, ind11:ind12) = [];
-            Log.ADC.Volts(:, ind11:ind12) = [];
+            Log.Frames.Time(frameInd1:frameInd2) = [];
+            Log.Frames.Position(frameInd1:frameInd2) = [];
+            Log.ADC.Time(:, ind1:ind2) = [];
+            Log.ADC.Volts(:, ind1:ind2) = [];
             
              %remove data corresponding to the amount of time in time2 from
             %data directly preceding the NEXT trial's start time (so remove
             %it from the end of the current trial)
             
-            ind21 = next_trial_start_time_ind - num_inds_erase2;
-            ind22 = next_trial_start_time_ind - 1;
-            frameInd21 = next_trial_frame_start_ind - frame_inds_erase2;
-            frameInd22 = next_trial_frame_start_ind - 1;
-            
-            Log.Frames.Time(frameInd21:frameInd22) = [];
-            Log.Frames.Position(frameInd21:frameInd22) = [];
-            Log.ADC.Time(:, ind21:ind22) = [];
-            Log.ADC.Volts(:, ind21:ind22) = [];
-
+           
+ 
 %             for chan = 1:4
 %                 Log.ADC.Time(chan, ind1:ind2) = [];
 %                 Log.ADC.Volts(chan, ind1:ind2) = [];
 % 
 %             end
             
-%            stop_times(time-1) = Log.ADC.Time(1, ind11-1);
+            stop_times(time-1) = Log.ADC.Time(1, ind1-1);
             
         end
     end
-    
-    for t = 2:length(start_times)
-        
-        start_ind = find(Log.ADC.Time(1,:)>=(start_times(time)*time_conv),1);
-        stop_times(time-1) = Log.ADC.Time(1, start_ind-1);
-    end
-    
+
     
 
             
