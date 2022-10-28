1cfd2302d485c887bf86371e6b8a9f66bc1c2554
Lisa Ferguson <taylorlm@outlook.com>
2022-10-28 16:49:16 -0400

    added notes to normalization functions

--- G4_Data_Analysis/support/data_processing_modules/get_max_process_normalization.m
+++ G4_Data_Analysis/support/data_processing_modules/get_max_process_normalization.m
@@ -4,8 +4,14 @@ function [maxs] = get_max_process_normalization(max_prctile, ...
 
     
 
-    datalen = numel(timeseries_data(1,:,:,:));
-    tmpdata = reshape(timeseries_data,[num_datatypes datalen]);
+    datalen = numel(timeseries_data(1,:,:,:)); % Get length of data
+    tmpdata = reshape(timeseries_data,[num_datatypes datalen]); %changes array shape so 
+    % that each channel has all data points in one array for that channel,
+    % so 7 x 400,000 ish
     maxs = repmat(prctile(tmpdata,max_prctile,2),[1 num_conds num_reps num_datapoints]);
+    %returns array the same size as timeseries_data but where every element
+    %is the max_prctile of the dataset. Usually set to 98th percentile. The
+    %percentile value is different for each channel so there are 7
+    %different values total.
 
 end
--- G4_Data_Analysis/support/data_processing_modules/normalize_ts_data.m
+++ G4_Data_Analysis/support/data_processing_modules/normalize_ts_data.m
@@ -1,10 +1,12 @@
 function [timeseries_data, maxVal] = normalize_ts_data(L_chan_idx, R_chan_idx, timeseries_data, maxs)
     
-    maxVal = max([maxs(L_chan_idx,1,1),maxs(R_chan_idx,1,1)]);
+    maxVal = max([maxs(L_chan_idx,1,1),maxs(R_chan_idx,1,1)]); %Gets the max value between the left and right channels
     datatypes = [L_chan_idx, R_chan_idx];
     for datatype = datatypes
         
         timeseries_data(datatype,:,:) = timeseries_data(datatype,:,:)./maxVal;
+        %Divide every data point in the left and right wing channels in
+        %timeseries_data by the max value. 
 
     end
     
