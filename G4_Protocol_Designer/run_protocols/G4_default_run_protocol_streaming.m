b00320ca794945546988332e451e72d1f32877c8
Lisa Ferguson <taylorlm@outlook.com>
2023-01-18 17:08:22 -0500

    Started simplifying streaming protocol -unfinished.

--- G4_Protocol_Designer/run_protocols/G4_default_run_protocol_streaming.m
+++ G4_Protocol_Designer/run_protocols/G4_default_run_protocol_streaming.m
@@ -65,103 +65,51 @@ end
 
 
  %% Set up parameters 
- %pretrial params-----------------------------------------------------
-     if isempty(p.pretrial{1}) %no need to set up pretrial params
-         pre_start = 0;
-     else %set up pretrial params here
-         pre_start = 1;
-         pre_mode = p.pretrial{1};
-         pre_pat = p.pretrial_pat_index;
-         pre_pos = p.pretrial_pos_index;
-         pre_ao_ind = p.pretrial_ao_indices;
+ params = assign_parameters(p);
 
-         if isempty(p.pretrial{8})
-             pre_frame_ind = 1;
-         elseif strcmp(p.pretrial{8},'r')
-             pre_frame_ind = 0; %use this later to randomize
-         else
-             pre_frame_ind = str2num(p.pretrial{8});
-         end
+ pre_start = params.pre_start;
+ pre_mode = params.pre_mode;
+ pre_pat = params.pre_pat;
+ pre_pos = params.pre_pos;
+ pre_ao_ind = params.pre_ao_ind;
+ pre_frame_ind = params.pre_frame_ind;
+ pre_frame_rate = params.pre_frame_rate;
+ pre_gain = params.pre_gain;
+ pre_offset = params.pre_offset;
+ pre_dur = params.pre_dur;
 
-         pre_frame_rate = p.pretrial{9};
-         pre_gain = p.pretrial{10};
-         pre_offset = p.pretrial{11};
-         pre_dur = p.pretrial{12};
-     end
- 
- %intertrial params---------------------------------------------------
- 
-     if isempty(p.intertrial{1})
-         inter_type = 0;%indicates whether or not there is an intertrial
-     else
-         inter_type = 1;
-         inter_mode = p.intertrial{1};
-         inter_pat = p.intertrial_pat_index;
-         inter_pos = p.intertrial_pos_index;
-         inter_ao_ind = p.intertrial_ao_indices;
+ inter_type = params.inter_type;
+ inter_mode = params.inter_mode;
+ inter_pat = params.inter_pat;
+ inter_pos = params.inter_pos;
+ inter_ao_ind = params.inter_ao_ind;
+ inter_frame_ind = params.inter_frame_ind;
+ inter_frame_rate = params.inter_frame_rate;
+ inter_gain = params.inter_gain;
+ inter_offset = params.inter_offset;
+ inter_dur = params.inter_dur;
+       
+ post_type = params.post_type;
+ post_mode = params.post_mode;
+ post_pat =params.post_pat;
+ post_pos = params.post_pos;
+ post_ao_ind = params.post_ao_ind;
+ post_frame_ind = params.post_frame_ind;
+ post_frame_rate = params.post_frame_rate;
+ post_gain = params.post_gain;
+ post_offset = params.post_offset;
+ post_dur = params.post_dur;
 
-         if isempty(p.intertrial{8})
-             inter_frame_ind = 1;
-         elseif strcmp(p.intertrial{8},'r')
-             inter_frame_ind = 0; %use this later to randomize
-         else
-             inter_frame_ind = str2num(p.intertrial{8});
-         end
+ block_trials = params.block_trials;
+ block_ao_indices = params.block_ao_indices;
+ reps = params.reps;
+ num_cond = params.num_cond;
 
-         inter_frame_rate = p.intertrial{9};
-         inter_gain = p.intertrial{10};
-         inter_offset = p.intertrial{11};
-         inter_dur = p.intertrial{12};
-     end
- 
- %posttrial params------------------------------------------------------
-     if isempty(p.posttrial{1})
-         post_type = 0;%indicates whether or not there is a posttrial
-     else
-         post_type = 1;
-         post_mode = p.posttrial{1};
-         post_pat = p.posttrial_pat_index;
-         post_pos = p.posttrial_pos_index;
-         post_ao_ind = p.posttrial_ao_indices;
+ aobits = params.aobits;
 
-         if isempty(p.posttrial{8})
-             post_frame_ind = 1;
-         elseif strcmp(p.posttrial{8},'r')
-             post_frame_ind = 0; %use this later to randomize
-         else
-             post_frame_ind = str2num(p.posttrial{8});
-         end
-
-         post_frame_rate = p.posttrial{9};
-         post_gain = p.posttrial{10};
-         post_offset = p.posttrial{11};
-         post_dur = p.posttrial{12};
-     end
- 
- %define static block trial params (will define the ones that change every
- %loop later)--------------------------------------------------------------
-     block_trials = p.block_trials; 
-     block_ao_indices = p.block_ao_indices;
-     reps = p.repetitions;
-     num_cond = length(block_trials(:,1)); %number of conditions
-     
 % Get input channels that are active for streaming
     
-    active_ai_streaming_channels = [];
-    if p.chan1_rate ~= 0
-        active_ai_streaming_channels(end+1) = 1;
-    end
-    if p.chan2_rate ~= 0
-        active_ai_streaming_channels(end+1) = 2;
-    end
-    if p.chan3_rate ~= 0
-        active_ai_streaming_channels(end+1) = 3;
-        
-    end
-    if p.chan4_rate ~= 0
-        active_ai_streaming_channels(end+1) = 4;
-    end
-    
+    aibits = get_active_streaming_channels(p); 
 
  %% Start host and switch to correct directory
     if ~isempty(ctlr)
@@ -195,10 +143,7 @@ end
     
  %% set active ao channels
      if ~isempty(p.active_ao_channels)
-         aobits = 0;
-        for bit = p.active_ao_channels
-            aobits = bitset(aobits,bit+1); %plus 1 bc aochans are 0-3
-        end
+        
         ctlr.setActiveAOChannels(dec2bin(aobits,4));
       
      end
@@ -206,10 +151,7 @@ end
 %% set active ai channels for streaming
     %set aibits for later telling panel_com which analog input channels are
     %streaming
-    aibits = 0;
-    for bit = active_ai_streaming_channels
-        aibits = bitset(aibits, bit);
-    end 
+    
     ctlr.setActiveAIChannels(aibits);
     
      
@@ -234,43 +176,11 @@ end
          
 %% Determine the total number of trials in order to define in what increments 
 %the progress bar will progress.-------------------------------------------
-            total_num_steps = 0; 
-            if pre_start == 1
-                total_num_steps = total_num_steps + 1;
-            end
-            if inter_type == 1
-                total_num_steps = total_num_steps + (reps*num_cond) - 1;
-                %Minus 1 because there is no intertrial before the first
-                %block trial OR after the last block trial.
-
-            end
-            if post_type == 1
-                total_num_steps = total_num_steps + 1;
-            end
-            total_num_steps = total_num_steps + (reps*num_cond);
-            %adds total number of block trials (not including intertrials)
+            total_num_steps = get_total_num_trials(params);
 
 %% Determine how long the experiment will take and update the title of the 
 %progress bar to reflect it------------------------------------------------
-            total_time = 0; 
-            if inter_type == 1
-                for i = 1:num_cond
-                    total_time = total_time + p.block_trials{i,12} + inter_dur;
-                end
-                total_time = (total_time * reps) - inter_dur; %bc no intertrial before first rep OR after last rep of the block.
-            else %meaning no intertrial
-                for i = 1:num_cond
-                    total_time = total_time + p.block_trials{i,12};
-                end
-                total_time = total_time * reps;
-            end
-
-            if pre_start == 1
-                total_time = total_time + pre_dur;
-            end
-            if post_type == 1
-                total_time = total_time + post_dur;
-            end
+            total_time = get_total_experiment_length(params);
             
             %Update the progress bar's label to reflect the expected
             %duration.
