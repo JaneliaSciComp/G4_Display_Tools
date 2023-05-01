e43cfceae3e925a5687f8644e88517f939b4b20b
Lisa Ferguson <taylorlm@outlook.com>
2023-05-01 16:01:58 -0400

    Updated the code in processing that loads the TDMS file to detect if there's more than one, and if so search for one with Final in the name. My processing will not work with multiple TDMS files as of now so if the user chose not to consolidate multiple TDMS files into one, then it will return a message and exit.

--- G4_Data_Analysis/support/data_processing_modules/load_tdms_log.m
+++ G4_Data_Analysis/support/data_processing_modules/load_tdms_log.m
@@ -2,15 +2,35 @@ function [Log] = load_tdms_log(exp_folder)
 
     %get files in provided directory
     files = dir(exp_folder);
-    
-    %Check if there are any files whose name contains 'G4_TDMS_Logs'
-    try
-        TDMS_logs_name = files(contains({files.name},{'G4_TDMS_Logs'})).name;
-    catch
-        error('cannot find G4_TDMS_Logs file in specified folder')
-       
-    end
-    load(fullfile(exp_folder,TDMS_logs_name),'Log');
+    TDMSfiles = files(contains({files.name},{'G4_TDMS_Logs'}));
 
+    if length(TDMSfiles) > 1
+    %Check if there are any files whose name contains 'G4_TDMS_Logs'
+        try
+            TDMS_logs_name = TDMSfiles(contains({TDMSfiles.name},{'G4_TDMS_Logs_Final'})).name;
+        catch
+            error('There are multiple TDMS Log files, but no combined final TDMS Log file')
+       
+        end
+
+    elseif length(TDMSfiles) == 1
+
+        try 
+            TDMS_logs_name = TDMSfiles(contains({TDMSfiles.name},{'G4_TDMS_Logs'})).name;
+        catch
+            error('Cannot find a TDMS Log file in the folder provided')
+
+        end
+
+    else
+
+        disp('Cannot find a TDMS Log file int he folder provided');
+        TDMS_logs_name = '';
+
+    end
+    
+    if ~isempty(TDMS_logs_name)
+        load(fullfile(exp_folder,TDMS_logs_name),'Log');
+    end
 
 end
