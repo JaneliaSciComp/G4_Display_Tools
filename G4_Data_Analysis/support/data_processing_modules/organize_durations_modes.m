140c6ec31a6351f58d408ab2db3c907783e346ba
Lisa Ferguson <taylorlm@outlook.com>
2023-07-17 16:08:35 -0400

    Processing now produces two more variables - cond_start_times and cond_gaps. The first gives the raw timestamp when the start-display command was received for each condition. The second gives the time between the end of the previous trial and the time the start-display was received for each condition.

--- G4_Data_Analysis/support/data_processing_modules/organize_durations_modes.m
+++ G4_Data_Analysis/support/data_processing_modules/organize_durations_modes.m
@@ -1,4 +1,5 @@
-function [cond_dur, cond_modes, cond_frame_move_time] = organize_durations_modes(num_conds, num_reps, ...
+function [cond_dur, cond_modes, cond_frame_move_time, cond_start_times, ...
+    cond_gaps] = organize_durations_modes(num_conds, num_reps, ...
     num_trials, exp_order, trial_stop_times, trial_start_times, ...
     trial_move_start_times, trial_modes, time_conv) 
     
@@ -8,6 +9,18 @@ function [cond_dur, cond_modes, cond_frame_move_time] = organize_durations_modes
     %Tells you t he time between the start of the trial (as defined by sending 
     %the 'Start-Display' command and the first movement of the pattern for each condition
     cond_frame_move_time = nan(num_conds, num_reps);
+
+    %Tells you the timestamp at which the start-display was received for
+    %each condition
+    cond_start_times = nan(num_conds, num_reps);
+
+    %Tells the you time that passed between the previous trial's end and
+    %the condition's start. This doesn't include intertrials so the gap
+    %should be expected to be the length of the intertrial plus a tiny
+    %amount.
+
+    cond_gaps = nan(num_conds, num_reps);
+    
     
     % Holds the mode of each condition
     cond_modes = nan(num_conds, num_reps);
@@ -18,6 +31,12 @@ function [cond_dur, cond_modes, cond_frame_move_time] = organize_durations_modes
         cond_dur(cond,rep) = double(trial_stop_times(trial) - trial_start_times(trial))/time_conv;
         cond_modes(cond,rep) = trial_modes(trial);
         cond_frame_move_time(cond, rep) = (double(trial_move_start_times(trial)) - double(trial_start_times(trial)))/(time_conv/1000);
+        cond_start_times(cond,rep) = trial_start_times(trial);
+        if trial == 1
+            cond_gaps(cond,rep) = 0;
+        else
+            cond_gaps(cond,rep) = double(trial_start_times(trial)) - double(trial_stop_times(trial-1));
+        end
     end
 
 end
