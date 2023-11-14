038259e1df76f0c3024dd917bcd24edff8d4efa0
Lisa Ferguson <taylorlm@outlook.com>
2023-11-14 15:00:50 -0500

    Added code to the convert_and_process_batch script. New code checks each folder to see if TDMS files are already present. If they are, it checks to see if the processed data file is present. Instead of the presence of these files causing an error, it will process the data if it hasn't been processed, or skip that folder entirely if the data is already processed. Note that it uses file name to check for the files, so if the names of the files have been changed for some reason, it may not work and an error may occur. Best to leave the TDMS and processed files with their default names.

--- G4_Data_Analysis/support/convert_and_process_batch.m
+++ G4_Data_Analysis/support/convert_and_process_batch.m
@@ -18,8 +18,8 @@
 % Conductor software, as it will use functions built into the Conductor.
 
 %% User must update these variables.
-date_folder = '';
-processing_settings_file = '';
+date_folder = 'C:\Users\taylo\Documents\Programming\Reiser\LPTephys_protocol01_upd_06-30-23_13-27-38\06_30_2023';
+processing_settings_file = 'C:\Users\taylo\Documents\Programming\Reiser\LPTephys_protocol01_upd_06-30-23_13-27-38\processing_settings.mat';
 
 %% Do not edit beyond this point
 
@@ -29,6 +29,7 @@ isub = [all(:).isdir];
 folder_names = {all(isub).name};
 folder_names(ismember(folder_names,{'.','..'})) = [];
 
+
 % Open up the Conductor's controller to access its functions
 run_con = G4_conductor_controller();
 disp("Running. Please do not close matlab...");
@@ -36,7 +37,50 @@ disp("Running. Please do not close matlab...");
 for fold = 1:length(folder_names)
 
     fly_results_folder = fullfile(date_folder, folder_names{fold});
+    
+    % Get folder and files names of the fly results folder so you can
+    % check to make sure the data hasn't already been processed.
+    all_fly = dir(fly_results_folder);
+    isub_fly = [all_fly(:).isdir];
+    folder_names_fly = {all_fly(isub_fly).name};
+    folder_names_fly(ismember(folder_names_fly,{'.','..'})) = [];
+    isafile = logical([]);
+    for i = 1:length(isub_fly)
+        if isub_fly(i) == 1
+            isafile(i) = false;
+        else
+            isafile(i) = true;
+        end
+    end
+    file_names = {all_fly(isafile).name};
+    file_names(ismember(file_names,{'.','..'})) = [];
+    tdms_present = 0;
+    processed_present = 0;
+    for file = 1:length(file_names)
+        if contains(file_names{file},'TDMS')
+            tdms_present = 1;
+        end
+        if contains(file_names{file}, 'Processed')
+            processed_present = 1;
+        end
+    end
 
+    % If any file was found in the folder containing "TDMS" in the name,
+    % then check for processed data. If it's not present, process the data
+    % assuming a settings file was provided. If it is also present, just
+    % skip this iteration entirely.
+    if tdms_present
+        if processed_present
+            continue;
+        else
+            if isfile(processing_settings_file)
+                process_data(fly_results_folder, processing_settings_file);
+            end
+            continue;
+        end
+    end
+
+    % At this point we know the data has not been converted or processed.
     % Check how many tdms folders are in the fly folder
     num_logs = run_con.check_number_logs(fly_results_folder);
     
