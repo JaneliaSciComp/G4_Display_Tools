f0a44a5ab6624c23b3f8e428bd25466f6acd6f20
Lisa Ferguson <lisaferguson@Lisas-MacBook-Pro.local>
2024-05-07 13:58:41 -0400

    Started a unit test file for the designer and conductor. Not working yet.

---
+++ G4_Protocol_Designer/support_files/tests/G4DisplayToolsTests.m
@@ -0,0 +1,42 @@
+classdef G4DisplayToolsTests < matlab.unittest.TestCase
+
+    properties
+        designer
+        conductor
+    end
+
+    methods(TestMethodSetup)
+        function startDesignerAndConductor(testCase)
+            testCase.designer = G4_designer_controller();
+            testCase.conductor = G4_conductor_controller();
+            testCase.conductor.layout()
+        end
+    end
+
+    methods(TestMethodTeardown)
+        function closeDesignerAndConductor(testCase)
+            testCase.designer.close_application();
+            testCase.conductor.view.close_application();
+        end
+    end
+
+    methods(Test, TestTags = {'conductor'})
+        
+        function openGoogleSheet(testCase)
+            try
+                testCase.conductor.open_google_sheet();
+                pass = true;
+            catch ME
+                pass = false;
+            end
+            testCase.verifyTrue(pass, 'Google Sheet failed to open');
+        end
+
+    end
+
+    methods(Test, TestTags = {'designer'})
+
+    end
+
+
+end
