9ff420a1005c64c1c07142d8257831b0fc5c175a
Lisa Ferguson <taylorlm@outlook.com>
2024-06-04 17:56:06 -0400

    Finished function to go through all flies and add the intertrial data to their ProcessedData files.

--- G4_Data_Analysis/support/get_intertrial_data.m
+++ G4_Data_Analysis/support/get_intertrial_data.m
@@ -18,66 +18,24 @@ function get_intertrial_data(experiment_folder, processing_settings)
     
     %Load settings (same for all flies)
     s = load(processing_settings);
+    channel_order = s.settings.channel_order;
     
     % Pull out all necessary settings that are the same accross flies
     %specify time ranges for parsing and data analysis
     data_rate = s.settings.data_rate; % rate (in Hz) which all data will be aligned to
     pre_dur = s.settings.pre_dur; %seconds before start of trial to include
-    post_dur = s.settings.post_dur; %seconds after end of trial to include
-    da_start = s.settings.da_start; %seconds after start of trial to start data analysis
-    da_stop = s.settings.da_stop; %seconds before end of trial to end data analysis
-    time_conv = s.settings.time_conv; %converts seconds to microseconds (TDMS timestamps are in micros)
-    common_cond_dur = s.settings.common_cond_dur; %sets whether all condition durations are the same (1) or not (0), for error-checking
-    processed_file_name = s.settings.processed_file_name;
-    hist_datatypes = s.settings.hist_datatypes; %{'Frame Position', 'LmR', 'LpR'};
+    post_dur = s.settings.post_dur; %seconds after end of trial to include    
+    time_conv = s.settings.time_conv; %converts seconds to microseconds (TDMS timestamps are in micros)    
     trial_options = s.settings.trial_options;
-    faLmR = s.settings.enable_faLmR;
-    condition_pairs = s.settings.condition_pairs;
-    enable_pos_series = s.settings.enable_pos_series;
-    pos_conditions = s.settings.pos_conditions;
-    num_positions = s.settings.num_positions;
-    data_pad = s.settings.data_pad;
-    sm_delay = s.settings.sm_delay;
     manual_first_start = s.settings.manual_first_start;
     combined_command = s.settings.combined_command;
-    max_prctile = s.settings.max_prctile;
     path_to_protocol = s.settings.path_to_protocol;
-    percent_to_shift = s.settings.percent_to_shift;
-    wbf_range = s.settings.wbf_range;
-    wbf_cutoff = s.settings.wbf_cutoff;
-    wbf_end_percent = s.settings.wbf_end_percent;
-
-    if isfield(s.settings, 'cross_correlation_tolerance')
-        corrTolerance = s.settings.cross_correlation_tolerance;
-    else
-        corrTolerance = .02;
-    end
-
-    if isfield(s.settings, 'flying')
-        flying = s.settings.flying;
-    else
-        flying = 1;
-    end
-
-    if isfield(s.settings, 'remove_nonflying_trials')
-        remove_nonflying_trials = s.settings.remove_nonflying_trials;
-    else
-        remove_nonflying_trials = 1;
-    end
 
     if isfield(s.settings, 'duration_diff_limit')
         duration_diff_limit = s.settings.duration_diff_limit;
     else
         duration_diff_limit = .1;
     end
-    
-    if isempty(s.settings.summary_save_path)
-        summary_save_path = exp_folder;
-    else
-        summary_save_path = s.settings.summary_save_path;
-    end
-    summary_filename = strcat(s.settings.summary_filename, '.txt');
-
 
     %Set which command we should be looking for in the log files
     if combined_command == 1
@@ -88,21 +46,8 @@ function get_intertrial_data(experiment_folder, processing_settings)
 
      %get indices for all datatypes - Any datatype not present in the
     %channel_order variable will return an empty index.
-    Frame_ind = strcmpi(channel_order,'Frame Position');
-    LmR_ind = find(strcmpi(channel_order,'LmR'));
-    LpR_ind = find(strcmpi(channel_order,'LpR'));
-    LmR_chan_idx = find(strcmpi(channel_order,'LmR_chan'));
-    L_chan_idx = find(strcmpi(channel_order,'L_chan'));
-    R_chan_idx = find(strcmpi(channel_order,'R_chan'));
-    F_chan_idx = find(strcmpi(channel_order,'F_chan'));
-    Current_idx = find(strcmpi(channel_order, 'current'));
-    Volt_idx = find(strcmpi(channel_order, 'voltage'));
-%    faLmR_ind = find(strcmpi(channel_order,'faLmR'));
-    num_ts_datatypes = length(channel_order);
-    num_ADC_chans = length(Log.ADC.Channels);
     
-
-
+    num_ts_datatypes = length(channel_order);
 
     %Loop through each date folder and each fly 
 
@@ -111,7 +56,7 @@ function get_intertrial_data(experiment_folder, processing_settings)
 
         allFlies = dir(fullfile(experiment_folder, date_folders{d}));
         isubFlies = [allFlies(:).isdir];
-        fly_folder_names = {all(isubFlies).name};
+        fly_folder_names = {allFlies(isubFlies).name};
         fly_folder_names(ismember(fly_folder_names,{'.','..'})) = [];
 
         for fly = 1:length(fly_folder_names)
@@ -185,13 +130,9 @@ function get_intertrial_data(experiment_folder, processing_settings)
                 end
             end
 
-            
+            save(fullfile(fly_folder, 'ProcessedData'), 'inter_ts_data', "-append");
 
         end
-
-
     end
-    
-
 
 end
