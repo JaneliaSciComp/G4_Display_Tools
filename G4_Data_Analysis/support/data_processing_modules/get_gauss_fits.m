4002b4502a1402b8ab0441bbc23af0379313cd26
Lisa Ferguson <taylorlm@outlook.com>
2024-12-09 11:03:22 -0500

    Added plotting of gaussian fits to the average condition data.

--- G4_Data_Analysis/support/data_processing_modules/get_gauss_fits.m
+++ G4_Data_Analysis/support/data_processing_modules/get_gauss_fits.m
@@ -1,6 +1,7 @@
 function [gaussVals, gaussFits] = get_gauss_fits(ts_data, medianVoltage, Volt_idx, exp_folder)
 
     fignum = 1;
+    ts_data_avg = squeeze(mean(ts_data,3, 'omitnan'));
 
     for cond = 1:size(ts_data,2)
         for rep = 1:size(ts_data,3)
@@ -23,6 +24,22 @@ function [gaussVals, gaussFits] = get_gauss_fits(ts_data, medianVoltage, Volt_id
             sgtitle(gauss_title);
 
         end
+        for frame = 1:size(ts_data,4)
+            avgRepVolt = mean(squeeze(ts_data_avg(Volt_idx, cond, frame, :)), 'omitnan');
+            gaussValsAvg{cond}(frame) = avgRepVolt - medianVoltage;
+        end
+        valsAvg = gaussValsAvg{cond};
+        valsAvg = valsAvg(~isnan(valsAvg));
+        xAvg = 2:length(valsAvg)+1;
+        yAvg = valsAvg;
+        gaussFitsAvg{cond} = fit(xAvg.', yAvg.', 'gauss1');
+
+        gauss_avg_title = ['Condition ' num2str(cond) ' Avg Over Reps Gaussian Fit'];
+        fig(fignum) = figure;
+        fignum = fignum + 1;
+        fAvg = gaussFitsAvg{cond};
+        plot(fAvg, xAvg, yAvg);
+        sgtitle(gauss_avg_title);
     end
 
     savefig(fig, fullfile(exp_folder, 'GaussianFits.fig'));
